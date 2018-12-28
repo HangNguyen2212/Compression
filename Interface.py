efd0ac5a8cd50c9a177d45368df8e1023db643b3
Nguyễn Thu Hằng <hangchan2212@gmail.com>
2018-12-28 22:30:26 +0700

    new

--- Interface.py
+++ Interface.py
@@ -25,24 +25,22 @@ def callback():
     w.configure(text = root.filename)
     filePath = root.filename
     
-    before = Image.open(root.filename)
+    basewidth = 300
+    img = Image.open(root.filename)
+    wpercent = (basewidth / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+    img.save('resized_image.jpg')
+    #resize img to fit label
+    before = Image.open('resized_image.jpg')
     bardejov = ImageTk.PhotoImage(before)
     label1 = Label(root,width= 300, heigh= 300, image=bardejov)
     label1.image = bardejov
     label1.place(x=20, y=380)
 
-    T1 = Label(root, text= "Image",font=("Helvetica", 15))
+    T1 = Label(root, text= "Before",font=("Helvetica", 15))
     T1.pack()
-    T1.place(x=150, y=340)
-    # T2 = Label(root, text= "After",font=("Helvetica", 20))
-    # T2.pack()
-    # T2.place(x=650, y=340)
-
-    # after = Image.open(root.filename)
-    # rotunda = ImageTk.PhotoImage(after)
-    # label2 = Label(root,width= 300, heigh= 300, image=rotunda)
-    # label2.image = rotunda
-    # label2.place(x=520, y=380)        
+    T1.place(x=150, y=340)     
 
 
 w = Label(root, text= "UNKNOWN FILE", bg="black", fg="white")
@@ -80,7 +78,10 @@ for val, language in enumerate(languages):
 
 #COMPRESSBTN
 def Compress():
-    window = tk.Toplevel(root)
+   
+    T2 = Label(root, text= "After",font=("Helvetica", 15))
+    T2.pack()
+    T2.place(x=650, y=340)   
     if v.get()== 0:
         pass
     elif v.get()== 1:
@@ -90,16 +91,6 @@ def Compress():
 
 btn = Button(root, text="Compress", command=Compress)
 btn.pack()
-
-
-
-
-# T1 = Label(root, text= "Before",font=("Helvetica", 28))
-# T1.pack()
-# T1.place(x=150, y=340)
-# T2 = Label(root, text= "After",font=("Helvetica", 28))
-# T2.pack()
-# T2.place(x=650, y=340)
 
 # before = Image.open(callback())
 # bardejov = ImageTk.PhotoImage(before)
